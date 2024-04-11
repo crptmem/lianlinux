8d6c93ca9088216f62f5658101794c1ef102bc94
crptmem <corrupt@kijy.de>
2024-04-11 16:14:42 +0300

    feat: do not require color arguments for `morph` and `rainbow` (fix #15)

--- src/cmd/mod.rs
+++ src/cmd/mod.rs
@@ -20,13 +20,13 @@ enum Command {
         mode: String,
 
         /// Red
-        red: u8,
+        red: Option<u8>,
 
         /// Green
-        green: u8,
+        green: Option<u8>,
 
         /// Blue
-        blue: u8
+        blue: Option<u8>
     },
 
     /// Run lianlinux as a daemon
@@ -34,26 +34,30 @@ enum Command {
 }
 
 /// # Change lightning mode by sending requests to daemon
-async fn light(mode: String, red: u8, green: u8, blue: u8) {
+async fn light(mode: String, red: Option<u8>, green: Option<u8>, blue: Option<u8>) {
+    if (mode == "static" || mode == "breathing") && (red.is_none() || green.is_none() || blue.is_none()) {
+        println!("Not enough arguments");
+        exit(1);
+    }
     match mode.as_str() {
         "static" => {
-            daemon::client::static_mode(red, blue, green).await;
+            daemon::client::static_mode(red.unwrap(), blue.unwrap(), green.unwrap()).await;
         },
         "breathing" => {
-            daemon::client::breathing_mode(red, blue, green).await;
+            daemon::client::breathing_mode(red.unwrap(), blue.unwrap(), green.unwrap()).await;
         },
         "rainbow" => {
-            daemon::client::rainbow_mode(red, blue, green).await;
+            daemon::client::rainbow_mode(0, 0, 0).await;
         },
         "morph" => {
-            daemon::client::morph_mode(red, blue, green).await;
+            daemon::client::morph_mode(0, 0, 0).await;
         },
         _ => {
             println!("Unknown mode {}", mode.red());
             exit(1);
         }
     }
-    println!("Changing mode to {} with colors {red}, {green}, {blue}", mode.purple());
+    //println!("Changing mode to {} with colors {red}, {green}, {blue}", mode.purple());
 } 
 
 /// # Handle command line arguments
